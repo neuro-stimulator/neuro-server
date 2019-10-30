d09e26c857a28834c543b57f363f6ffa7302d520
petr <stechy12@gmail.com>
2019-10-30 14:28:12 +0100

    Přidání zprávy s informací o úspěšnosti vykonání akce.

--- src/experiments/experiments.controller.ts
+++ src/experiments/experiments.controller.ts
@@ -35,21 +35,21 @@ export class ExperimentsController {
   public async insert(@Body() body: Experiment): Promise<ResponseObject<Experiment>> {
     const experiment: Experiment = await this._service.insert(body);
     this._gateway.insert(experiment);
-    return {data: experiment};
+    return {data: experiment, message: {text: 'Experiment byl úspěšně vytvořen.', type: 0}};
   }
 
   @Patch()
   public async update(@Body() body: Experiment): Promise<ResponseObject<Experiment>> {
     const experiment: Experiment = await this._service.update(body);
     this._gateway.update(experiment);
-    return {data: experiment};
+    return {data: experiment, message: {text: 'Experiment byl úspěšně aktualizován.', type: 0}};
   }
 
   @Delete(':id')
   public async delete(@Param() params: {id: number}): Promise<ResponseObject<Experiment>> {
     const experiment: Experiment = await this._service.delete(params.id);
     this._gateway.delete(experiment);
-    return {data: experiment};
+    return {data: experiment, message: {text: 'Experiment byl úspěšně odstraněn.', type: 0}};
   }
 
 }
