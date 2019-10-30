bc54561a8590ce6d8cd13e7defc14ae3ea174bbd
petr <stechy12@gmail.com>
2019-10-30 15:29:45 +0100

    Zrušení podpory migrací.

--- src/database.config.ts
+++
@@ -1,8 +0,0 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-export const DATABASE_CONFIG: TypeOrmModuleOptions = {
-  type: 'sqlite',
-  database: '../dist/database.sqlite',
-  entities: ['../dist/**/*.entity{.ts,.js}'],
-  synchronize: true,
-};
