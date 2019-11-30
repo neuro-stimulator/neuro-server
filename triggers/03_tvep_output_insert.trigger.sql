c20669f1f6d4c503e719d7036b628ede20a07d01
petr <stechy12@gmail.com>
2019-11-30 11:52:56 +0100

    Výchozí svítivost výstupů je nyní na 100%.

--- triggers/03_tvep_output_insert.trigger.sql
+++ triggers/03_tvep_output_insert.trigger.sql
@@ -2,14 +2,13 @@ CREATE TRIGGER IF NOT EXISTS tvep_output_insert AFTER INSERT
     ON experiment_tvep_entity
 BEGIN
 
-    INSERT INTO experiment_tvep_output_entity(experimentId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 0, 0, 0, 0, 0);
-    INSERT INTO experiment_tvep_output_entity(experimentId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 1, 0, 0, 0, 0);
-    INSERT INTO experiment_tvep_output_entity(experimentId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 2, 0, 0, 0, 0);
-    INSERT INTO experiment_tvep_output_entity(experimentId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 3, 0, 0, 0, 0);
-    INSERT INTO experiment_tvep_output_entity(experimentId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 4, 0, 0, 0, 0);
-    INSERT INTO experiment_tvep_output_entity(experimentId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 5, 0, 0, 0, 0);
-    INSERT INTO experiment_tvep_output_entity(experimentId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 6, 0, 0, 0, 0);
-    INSERT INTO experiment_tvep_output_entity(experimentId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 7, 0, 0, 0, 0);
-    INSERT INTO experiment_tvep_output_entity(experimentId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 8, 0, 0, 0, 0);
+    INSERT INTO experiment_tvep_output_entity(experimentId, orderId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 0, 1, 0, 0, 0, 100);
+    INSERT INTO experiment_tvep_output_entity(experimentId, orderId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 1, 1, 0, 0, 0, 100);
+    INSERT INTO experiment_tvep_output_entity(experimentId, orderId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 2, 1, 0, 0, 0, 100);
+    INSERT INTO experiment_tvep_output_entity(experimentId, orderId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 3, 1, 0, 0, 0, 100);
+    INSERT INTO experiment_tvep_output_entity(experimentId, orderId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 4, 1, 0, 0, 0, 100);
+    INSERT INTO experiment_tvep_output_entity(experimentId, orderId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 5, 1, 0, 0, 0, 100);
+    INSERT INTO experiment_tvep_output_entity(experimentId, orderId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 6, 1, 0, 0, 0, 100);
+    INSERT INTO experiment_tvep_output_entity(experimentId, orderId, patternLength, pattern, out, wait, brightness) VALUES (new.id, 7, 1, 0, 0, 0, 100);
 
 END;
