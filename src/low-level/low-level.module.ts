6155ddfaeec6fbc3f18cc07ab4445ecc6c8ccb7f
petr <stechy12@gmail.com>
2020-04-08 17:38:09 +0200

    Opravení názvu souboru.

--- src/low-level/low-level.module.ts
+++ src/low-level/low-level.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 
-import * as isCi from 'is-ci';
-
 import { FileBrowserService } from '../file-browser/file-browser.service';
 import { SettingsModule } from '../settings/settings.module';
-import { SettingsService } from '../settings/settings.service';
-import { FakeSerialService } from './fake-serial.service';
-import { RealSerialService } from './real-serial.service';
 import { SerialService } from './serial.service';
 import { SerialGateway } from './serial.gateway';
 import { LowLevelController } from './low-level.controller';
-import { serialProvider } from './serialProvider';
+import { serialProvider } from './serial-provider';
 
 @Module({
   controllers: [
