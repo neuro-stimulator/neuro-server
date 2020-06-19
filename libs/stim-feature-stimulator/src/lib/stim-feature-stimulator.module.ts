74c4107b704e91b128c668f93ee1dd6fac41b5d4
petr <stechy12@gmail.com>
2020-06-19 17:08:11 +0200

    Odstranění zakomentovaných věcí.

--- libs/stim-feature-stimulator/src/lib/stim-feature-stimulator.module.ts
+++ libs/stim-feature-stimulator/src/lib/stim-feature-stimulator.module.ts
@@ -1,52 +1,11 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 
-import { StimLibSocketModule } from '@diplomka-backend/stim-lib-socket';
-import { StimFeatureFileBrowserModule } from '@diplomka-backend/stim-feature-file-browser';
-import { StimFeatureExperimentsModule } from '@diplomka-backend/stim-feature-experiments';
-
-import { StimulatorEvents } from './application/events';
-import { StimulatorQueries } from './application/queries';
-import { SerialHandlers } from './application/commands';
-import { StimulatorSagas } from './application/sagas';
-import { StimulatorService } from './domain/service/stimulator.service';
-import { SerialFacade } from './infrastructure/service/serial.facade';
 import { StimulatorFacade } from './infrastructure/service/stimulator.facade';
-import { SerialController } from './infrastructure/controllers/serial.controller';
-import { StimulatorController } from './infrastructure/controllers/stimulator.controller';
-import { FakeSerialResponder } from './domain/service/serial/fake/fake-serial-responder';
-import { serialProvider } from './domain/provider/serial-provider';
-import { DefaultFakeSerialResponder } from './domain/service/serial/fake/fake-serial.positive-responder';
-import { StimulatorModuleConfig } from 'libs/stim-feature-stimulator/src/lib/domain/model/stimulator-module-config';
-import { StimFeatureStimulatorCoreModule } from 'libs/stim-feature-stimulator/src/lib/stim-feature-stimulator-core.module';
+import { StimulatorModuleConfig } from './domain/model/stimulator-module-config';
+import { StimFeatureStimulatorCoreModule } from './stim-feature-stimulator-core.module';
 
-@Module({
-  /*
-  controllers: [SerialController, StimulatorController],
-  imports: [
-    CqrsModule,
-    StimLibSocketModule,
-    StimFeatureFileBrowserModule.forFeature(),
-    StimFeatureExperimentsModule,
-  ],
-  providers: [
-    {
-      provide: FakeSerialResponder,
-      useClass: DefaultFakeSerialResponder,
-    },
-    serialProvider,
-    StimulatorService,
-    SerialFacade,
-    StimulatorFacade,
-
-    ...SerialHandlers,
-    ...StimulatorQueries,
-    ...StimulatorEvents,
-    ...StimulatorSagas,
-  ],
-  exports: [StimulatorFacade],
-  */
-})
+@Module({})
 export class StimFeatureStimulatorModule {
   static forRoot(config: StimulatorModuleConfig): DynamicModule {
     return {
