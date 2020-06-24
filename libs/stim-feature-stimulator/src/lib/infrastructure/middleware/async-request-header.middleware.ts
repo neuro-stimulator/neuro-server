2c20f15281d085c2df177fac372e4e1efa8a18e2
petr <stechy12@gmail.com>
2020-06-24 17:40:09 +0200

    Odstranění logu do konzole.

--- libs/stim-feature-stimulator/src/lib/infrastructure/middleware/async-request-header.middleware.ts
+++ libs/stim-feature-stimulator/src/lib/infrastructure/middleware/async-request-header.middleware.ts
@@ -3,8 +3,6 @@ import { IncomingMessage, ServerResponse } from 'http';
 
 export class AsyncRequestHeaderMiddleware implements NestMiddleware {
   use(req: IncomingMessage, res: ServerResponse, next: () => void): any {
-    console.log('APlikuji middleware');
-
     let accessControlHeaders = res.getHeader('Access-Control-Allow-Headers');
     accessControlHeaders += ', Async-Request';
     res.setHeader('Access-Control-Allow-Headers', accessControlHeaders);
