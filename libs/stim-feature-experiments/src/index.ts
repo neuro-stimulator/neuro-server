b92a7ed73a7b9da1afac05883abad59e7241081e
petr <stechy12@gmail.com>
2020-07-09 15:54:54 +0200

    Odstranění souborů ze starých modulů.

--- libs/stim-feature-experiments/src/index.ts
+++
@@ -1,7 +0,0 @@
-// export * from './lib/stim-feature-experiments.module';
-//
-// export { ENTITIES } from '../domain/src/lib/model/entity';
-//
-// export { ExperimentsFacade } from '../infrastructure/src/lib/service/experiments.facade';
-//
-// export { ExperimentEntity } from '../domain/src/lib/model/entity/experiment.entity';
--- libs/stim-feature-stimulator/README.md
+++
@@ -1,7 +0,0 @@
-# stim-feature-stimulator
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `ng test stim-feature-stimulator` to execute the unit tests via [Jest](https://jestjs.io).
