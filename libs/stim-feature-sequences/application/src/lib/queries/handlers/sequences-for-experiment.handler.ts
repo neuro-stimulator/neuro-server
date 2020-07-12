0fbd9769931fe516369abb51036fc3f77c8f7312
petr <stechy12@gmail.com>
2020-07-12 16:28:52 +0200

    Opravení filtru pro nalezení sekvencí podle zadaného experimentu.

--- libs/stim-feature-sequences/application/src/lib/queries/handlers/sequences-for-experiment.handler.ts
+++ libs/stim-feature-sequences/application/src/lib/queries/handlers/sequences-for-experiment.handler.ts
@@ -11,7 +11,7 @@ export class SequencesForExperimentHandler implements IQueryHandler<SequencesFor
 
   async execute(query: SequencesForExperimentQuery): Promise<Sequence[]> {
     return this.service.findAll({
-      where: { experimentID: query.experimentID },
+      where: { experimentId: query.experimentID },
     });
   }
 }
