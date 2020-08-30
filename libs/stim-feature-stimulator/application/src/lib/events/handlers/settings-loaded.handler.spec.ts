82c3c6a4772718bca1fbfe039510827396b0759b
petr <stechy12@gmail.com>
2020-08-30 09:12:10 +0200

    Přidání nových testů do modulu se stimulátorem.

---
+++ libs/stim-feature-stimulator/application/src/lib/events/handlers/settings-loaded.handler.spec.ts
@@ -0,0 +1,69 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { Settings, SettingsFacade, SettingsWasLoadedEvent } from '@diplomka-backend/stim-feature-settings';
+
+import { commandBusProvider, MockType } from 'test-helpers/test-helpers';
+
+import { SettingsLoadedHandler } from './settings-loaded.handler';
+import { CommandBus } from '@nestjs/cqrs';
+import { OpenCommand } from '@diplomka-backend/stim-feature-stimulator/application';
+
+describe('SettingsLoadedHandler', () => {
+  let testingModule: TestingModule;
+  let handler: SettingsLoadedHandler;
+  let facade: MockType<SettingsFacade>;
+  let commandBus: MockType<CommandBus>;
+
+  beforeEach(async () => {
+    testingModule = await Test.createTestingModule({
+      providers: [
+        SettingsLoadedHandler,
+        {
+          provide: SettingsFacade,
+          useFactory: jest.fn(() => ({ getSettings: jest.fn(), updateSettings: jest.fn() })),
+        },
+        commandBusProvider,
+      ],
+    }).compile();
+
+    handler = testingModule.get<SettingsLoadedHandler>(SettingsLoadedHandler);
+    // @ts-ignore
+    facade = testingModule.get<MockType<SettingsFacade>>(SettingsFacade);
+    // @ts-ignore
+    commandBus = testingModule.get<MockType<CommandBus>>(CommandBus);
+  });
+
+  it('positive - should not open serial port automaticaly', async () => {
+    const settings: Settings = {};
+    settings.autoconnectToStimulator = false;
+    const event = new SettingsWasLoadedEvent(settings);
+
+    await handler.handle(event);
+
+    expect(facade.updateSettings).not.toBeCalled();
+    expect(commandBus.execute).not.toBeCalled();
+  });
+
+  it('positive - should disable automatic port open when path is undefined', async () => {
+    const settings: Settings = {};
+    settings.autoconnectToStimulator = true;
+    const event = new SettingsWasLoadedEvent(settings);
+
+    await handler.handle(event);
+
+    expect(facade.updateSettings).toBeCalledWith({ ...settings, autoconnectToStimulator: false });
+    expect(commandBus.execute).not.toBeCalled();
+  });
+
+  it('positive - should automaticaly open serial port', async () => {
+    const settings: Settings = {};
+    settings.autoconnectToStimulator = true;
+    settings.comPortName = 'comPortName';
+    const event = new SettingsWasLoadedEvent(settings);
+
+    await handler.handle(event);
+
+    expect(facade.updateSettings).not.toBeCalled();
+    expect(commandBus.execute).toBeCalledWith(new OpenCommand(settings.comPortName));
+  });
+});
