0b36a90cd3600a08b232e92898cfa4eeda9020e7
petr <stechy12@gmail.com>
2020-10-10 17:10:14 +0200

    Automatické odeslání celého stavu přehrávače experimentů po úspěšné inicializace a po vymazání.

--- libs/stim-feature-player/application/src/lib/commands/handlers/experiment-result-clear.handler.ts
+++ libs/stim-feature-player/application/src/lib/commands/handlers/experiment-result-clear.handler.ts
@@ -1,18 +1,20 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandBus, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { Logger } from '@nestjs/common';
 
 import { PlayerService } from '../../service/player.service';
 import { ExperimentResultClearCommand } from '../impl/experiment-result-clear.command';
+import { SendPlayerStateToClientCommand } from '../impl/to-client/send-player-state-to-client.command';
 
 @CommandHandler(ExperimentResultClearCommand)
 export class ExperimentResultClearHandler implements ICommandHandler<ExperimentResultClearCommand, void> {
   private readonly logger: Logger = new Logger(ExperimentResultClearHandler.name);
 
-  constructor(private readonly service: PlayerService) {}
+  constructor(private readonly service: PlayerService, private readonly commandBus: CommandBus) {}
 
   async execute(command: ExperimentResultClearCommand): Promise<void> {
     this.logger.debug('Budu mazat aktuální výsledek experimentu.');
 
     await this.service.clearRunningExperimentResult();
+    await this.commandBus.execute(new SendPlayerStateToClientCommand(this.service.playerConfiguration));
   }
 }
--- libs/stim-feature-player/application/src/lib/event/handlers/player-experiment-result-was-initialized.handler.ts
+++ libs/stim-feature-player/application/src/lib/event/handlers/player-experiment-result-was-initialized.handler.ts
@@ -1,13 +1,19 @@
-import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import { CommandBus, EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { Logger } from '@nestjs/common';
 
+import { PlayerService } from '../../service/player.service';
+import { SendPlayerStateToClientCommand } from '../../commands/impl/to-client/send-player-state-to-client.command';
 import { ExperimentResultWasInitializedEvent } from '../impl/experiment-result-was-initialized.event';
 
 @EventsHandler(ExperimentResultWasInitializedEvent)
 export class PlayerExperimentResultWasInitializedHandler implements IEventHandler<ExperimentResultWasInitializedEvent> {
   private readonly logger: Logger = new Logger(PlayerExperimentResultWasInitializedHandler.name);
 
+  constructor(private readonly service: PlayerService, private readonly commandBus: CommandBus) {}
+
   async handle(event: ExperimentResultWasInitializedEvent): Promise<any> {
     this.logger.debug('Výsledek experimentu byl úspěšně inicializován.');
+
+    await this.commandBus.execute(new SendPlayerStateToClientCommand(this.service.playerConfiguration));
   }
 }
