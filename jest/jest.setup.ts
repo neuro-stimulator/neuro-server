be085d486c14df7c43b124ebf7477ec052c7d35e
petr <stechy12@gmail.com>
2020-12-08 18:02:48 +0100

    Odstranění nastavení loggeru z globálního nastavení testů.

--- jest/jest.setup.ts
+++ jest/jest.setup.ts
@@ -1,9 +1,3 @@
-import { Logger } from '@nestjs/common/services';
-
-import { createNoOpLogger } from '../test-helpers/test-helpers';
-
 export const setup = async () => {
   console.log('Global setup!');
-
-  Logger.overrideLogger(createNoOpLogger());
 };
