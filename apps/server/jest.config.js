3efa9510763de358dcc37cd5bfe6179884c8554f
petr <stechy12@gmail.com>
2020-12-22 11:33:09 +0100

    Změny v konfiguraci testů.

--- apps/server/jest.config.js
+++ apps/server/jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   name: 'server',
-  preset: './jest.config.js',
+  preset: '../../jest.config.js',
   testMatch: ['**/*.e2e-spec.ts'],
-  coverageDirectory: './coverage/apps/server',
+  coverageDirectory: '../../coverage/apps/server',
   coveragePathIgnorePatterns: [
     'src/app/database',
     'src/app/middleware',
     'src/environments',
     'test/helpers'
   ],
-  globals: { 'ts-jest': { tsconfig: 'tsconfig.spec.json' } },
+  globals: { 'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' } },
 };
