fbf602fd473491dce107a6ab23bb76a17d11b7fc
petr <stechy12@gmail.com>
2020-12-29 14:03:44 +0100

    Opravení triggerů.

--- apps/server/triggers/031_cvep_output_insert.trigger.sql
+++ apps/server/triggers/031_cvep_output_insert.trigger.sql
@@ -1,5 +1,5 @@
-CREATE TRIGGER IF NOT EXISTS fvep_output_insert AFTER INSERT
-    ON experiment_fvep_entity
+CREATE TRIGGER IF NOT EXISTS cvep_output_insert AFTER INSERT
+    ON experiment_cvep_entity
 BEGIN
 
     {BEGIN}INSERT INTO experiment_cvep_output_entity(experimentId, orderId, type, brightness, x, y, width, height, manualAlignment, horizontalAlignment, verticalAlignment) VALUES (new.id, {INDEX}, 1, 100, null, null, null, null, null, null, null);{END}
--- apps/server/triggers/041_rea_output_insert.trigger.sql
+++ apps/server/triggers/041_rea_output_insert.trigger.sql
@@ -1,5 +1,5 @@
-CREATE TRIGGER IF NOT EXISTS fvep_output_insert AFTER INSERT
-    ON experiment_fvep_entity
+CREATE TRIGGER IF NOT EXISTS rea_output_insert AFTER INSERT
+    ON experiment_rea_entity
 BEGIN
 
     {BEGIN}INSERT INTO experiment_rea_output_entity(experimentId, orderId, type, brightness, x, y, width, height, manualAlignment, horizontalAlignment, verticalAlignment) VALUES (new.id, {INDEX}, 1, 100, null, null, null, null, null, null, null);{END}
