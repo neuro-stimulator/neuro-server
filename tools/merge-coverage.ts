5f7fe51e938b1c3ee9c54e79a0ede4e8b4e294ec
petr <stechy12@gmail.com>
2021-01-03 19:59:25 +0100

    Aktualizace knihovny pro coverage merge.

--- tools/merge-coverage.ts
+++ tools/merge-coverage.ts
@@ -1,30 +1,34 @@
-/* SOURCE: https://github.com/facebook/jest/issues/2418#issuecomment-478932514 */
-/* tslint:disable:no-console */
-/*
-ts-node ./merge-coverage.ts --report ./coverage0/coverage-final.json --report ./coverage1/coverage-final.json
-*/
+import * as path from 'path';
+import * as fs from 'fs';
 
-import * as fs from 'fs-extra';
-import { createReporter } from 'istanbul-api';
-import { createCoverageMap } from 'istanbul-lib-coverage';
+import * as libCoverage from 'istanbul-lib-coverage';
+import * as libReport from 'istanbul-lib-report';
+import * as reports from 'istanbul-reports';
 
 import { readDirectoryRecursive } from './tools-helper';
 
 async function main() {
   const reportFiles = readDirectoryRecursive('coverage', 'coverage-final.json', '');
-  const reporters = ['json', 'lcov'];
 
-  const map = createCoverageMap({});
+  const map = libCoverage.createCoverageMap();
 
   reportFiles.forEach((file) => {
-    const r = fs.readJsonSync(file);
-    map.merge(r);
+    const finalReport: Record<string, libCoverage.FileCoverageData> = JSON.parse(fs.readFileSync(path.join(__dirname, '..', file), { encoding: 'utf8' }));
+    Object.values(finalReport).forEach((fileReport) => {
+      map.addFileCoverage(fileReport);
+    });
   });
 
-  const reporter = createReporter();
-  await reporter.addAll(reporters);
-  reporter.write(map);
-  console.log('Created a merged coverage report in ./coverage');
+  const context = libReport.createContext({
+    dir: './coverage',
+    coverageMap: map,
+  });
+
+  const jsonSummary: libReport.ReportBase = (reports.create('json-summary') as unknown) as libReport.ReportBase;
+  const lcov: libReport.ReportBase = (reports.create('lcov', {}) as unknown) as libReport.ReportBase;
+
+  jsonSummary.execute(context);
+  lcov.execute(context);
 }
 
 main().catch((err) => {
