48cd32fc6a846fe27bbb3fee72f061a45f6b4e55
petr <stechy12@gmail.com>
2021-11-10 16:45:34 +0100

    Opravení výchozích hodnot u výstupů experimentu.

--- apps/server/triggers/031_cvep_output_insert.trigger.sql
+++ apps/server/triggers/031_cvep_output_insert.trigger.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER IF NOT EXISTS cvep_output_insert AFTER INSERT
     WHEN (SELECT enabled FROM trigger_control WHERE trigger_control.name = 'cvep_output_insert')=1
 BEGIN
 
-    {BEGIN}INSERT INTO experiment_cvep_output_entity(experimentId, orderId, type, brightness, x, y, width, height, manualAlignment, horizontalAlignment, verticalAlignment) VALUES (new.id, {INDEX}, 1, 100, 0, 0, null, null, false, 1, 1);{END}
+    {BEGIN}INSERT INTO experiment_cvep_output_entity(experimentId, orderId, type, brightness, x, y, width, height, manualAlignment, horizontalAlignment, verticalAlignment) VALUES (new.id, {INDEX}, 1, 100, 0, 0, 0, 0, false, 1, 1);{END}
 
 END;
 
--- apps/server/triggers/041_rea_output_insert.trigger.sql
+++ apps/server/triggers/041_rea_output_insert.trigger.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER IF NOT EXISTS rea_output_insert AFTER INSERT
     WHEN (SELECT enabled FROM trigger_control WHERE trigger_control.name = 'rea_output_insert')=1
 BEGIN
 
-    {BEGIN}INSERT INTO experiment_rea_output_entity(experimentId, orderId, type, brightness, x, y, width, height, manualAlignment, horizontalAlignment, verticalAlignment) VALUES (new.id, {INDEX}, 1, 100, 0, 0, null, null, false, 1, 1);{END}
+    {BEGIN}INSERT INTO experiment_rea_output_entity(experimentId, orderId, type, brightness, x, y, width, height, manualAlignment, horizontalAlignment, verticalAlignment) VALUES (new.id, {INDEX}, 1, 100, 0, 0, 0, 0, false, 1, 1);{END}
 
 END;
 
