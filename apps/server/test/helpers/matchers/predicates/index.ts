6168701714780bdc95d0106319f1646d8a5a139f
petr <stechy12@gmail.com>
2022-01-09 16:02:55 +0100

    Odstranění nepotřebného predikátu.

--- apps/server/test/helpers/matchers/predicates/index.ts
+++ apps/server/test/helpers/matchers/predicates/index.ts
@@ -1,6 +1,5 @@
 export * from './predicate';
 
-export * from './output-type.predicate';
 export * from './used-outputs.predicate';
 export * from './user-groups.perdicate';
 export * from './standard.predicate';
--- apps/server/test/helpers/matchers/predicates/output-type.predicate.ts
+++
@@ -1,3 +0,0 @@
-export const outputType = (lhs, rhs) => {
-  return true;
-};
